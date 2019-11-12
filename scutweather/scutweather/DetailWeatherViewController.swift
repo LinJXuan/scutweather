22d6a58e625cc475fdcef74d683f3e0f0c3f5dcb
ysl <944266726@qq.com>
2019-11-12 14:55:07 +0800

    DetailWeatherViewController
    DetailWeatherViewController

---
+++ scutweather/scutweather/DetailWeatherViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailWeatherViewController.swift
+//  scutweather
+//
+//  Created by Apple on 2019/11/12.
+//  Copyright © 2019 scut_flyc. All rights reserved.
+//
+
+import UIKit
+
+class DetailWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
