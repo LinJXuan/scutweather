f79bb0be9b1ad1f2c0feb51af2a946169247572c
clement <1207717200@qq.com>
2019-11-26 15:43:53 +0800

    cjl1

---
+++ scutweather/scutweather/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  scutweather
+//
+//  Created by clement on 2019/11/26.
+//  Copyright © 2019 scut_flyc. All rights reserved.
+//
+
+import Foundation
--- scutweather/scutweather/ViewController2.swift
+++ scutweather/scutweather/ViewController2.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController2: UIViewController {
+    
     @IBOutlet weak var tem4: UILabel!
     @IBOutlet weak var tem3: UILabel!
     @IBOutlet weak var tem2: UILabel!
@@ -33,54 +33,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        //使用说明
-//        let http=httpConnection()
-//
-//        //天气预报
-//        var WT:[weather] = [weather]()
-//        WT=http.seacherWeather(city: "输入城市")
-//        var wt : weather
-//        wt=WT[0]
-//        print(wt.temperature)
-//
-//        //指数
-//        var index:index
-//        index=http.getIndex()
-//        print(index.dressing_advice)
-//
-//        //空气质量
-//        var quality:String
-//        quality=http.seacherQuality(city: "输入城市")
-//        print(quality)
-   
-        
-        day1.text="多云"
-        day2.text="晴天"
-        day3.text="晴天"
-        day4.text="晴天"
-//        if day1.text=="晴天" {
-//        let path=Bundle.main.path(forResource: "sun", ofType: "jpg")
-//        let fileimage=UIImage(contentsOfFile: path!)
-//        mainpic.image=fileimage
-//        }
-//        else if day1.text=="多云" {
-//            let path=Bundle.main.path(forResource: "cloud", ofType: "jpg")
-//            let fileimage=UIImage(contentsOfFile: path!)
-//            mainpic.image=fileimage
-//        }
-//         else if day1.text=="雷暴雨" {
-//            let path=Bundle.main.path(forResource: "lighting", ofType: "jpg")
-//            let fileimage=UIImage(contentsOfFile: path!)
-//            mainpic.image=fileimage
-//        }
-//        else if day1.text=="小雨" {
-//            let path=Bundle.main.path(forResource: "rain", ofType: "jpg")
-//            let fileimage=UIImage(contentsOfFile: path!)
-//            mainpic.image=fileimage
-//        }
+        //        //使用说明
+        //        let http=httpConnection()
+        //
+        //        //天气预报
+        //        var WT:[weather] = [weather]()
+        //        WT=http.seacherWeather(city: "输入城市")
+        //        var wt : weather
+        //        wt=WT[0]
+        //        print(wt.temperature)
+        //
+        //        //指数
+        //        var index:index
+        //        index=http.getIndex()
+        //        print(index.dressing_advice)
+        //
+        //        //空气质量
+        //        var quality:String
+        //        quality=http.seacherQuality(city: "输入城市")
+        //        print(quality)
         let http=httpConnection()
         var WT:[weather] = [weather]()
-        WT=http.seacherWeather(city: "广州")
+        WT=http.seacherWeather(city: "惠州")
         wt1=WT[0]
         wt2=WT[0]
         wt3=WT[0]
@@ -90,16 +64,16 @@ class ViewController: UIViewController {
         day3.text=wt3?.weather
         day4.text=wt4?.weather
         if day1.text=="晴天" {
-        let path=Bundle.main.path(forResource: "sun", ofType: "jpg")
-        let fileimage=UIImage(contentsOfFile: path!)
-        mainpic.image=fileimage
+            let path=Bundle.main.path(forResource: "sun", ofType: "jpg")
+            let fileimage=UIImage(contentsOfFile: path!)
+            mainpic.image=fileimage
         }
         else if day1.text=="多云" {
             let path=Bundle.main.path(forResource: "cloud", ofType: "jpg")
             let fileimage=UIImage(contentsOfFile: path!)
             mainpic.image=fileimage
         }
-         else if day1.text=="雷暴雨" {
+        else if day1.text=="雷暴雨" {
             let path=Bundle.main.path(forResource: "lighting", ofType: "jpg")
             let fileimage=UIImage(contentsOfFile: path!)
             mainpic.image=fileimage
